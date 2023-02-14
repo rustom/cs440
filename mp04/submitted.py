c8ec0991244363d1f54e0fc044ab0ae0826395d6
Rustom Ichhaporia <45826892+rustom@users.noreply.github.com>
2023-02-14 17:29:42 -0600

    Complete mp 04

--- mp04/submitted.py
+++ mp04/submitted.py
@@ -25,7 +25,25 @@ class NeuralNet(torch.nn.Module):
         super().__init__()
         ################# Your Code Starts Here #################
 
-        raise NotImplementedError("You need to write this part!")
+        # create a three-layer network with around 300 hidden neurons
+        # In each training iteration, the input of your network is a batch of preprocessed image data of size (batch size, 2883)
+
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 3 * 3, 300)
+        self.fc2 = nn.Linear(300, 5)
+        self.relu = nn.ReLU()
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+
+        # self.fc1 = nn.Linear(2883, 200)
+        # self.fc2 = nn.Linear(200, 200)
+        # self.fc3 = nn.Linear(200, 5)
+        # self.relu = nn.ReLU()
+        # self.logsoftmax = nn.LogSoftmax(dim=1)
+
         ################## Your Code Ends here ##################
 
     def forward(self, x):
@@ -39,9 +57,24 @@ class NeuralNet(torch.nn.Module):
             y:      an (N, output_size) tensor of output from the network
         """
         ################# Your Code Starts Here #################
+        # print(x.shape) 100, 2883
+        # x = x.reshape(100, 3, 31, 31)
+        # x = self.relu(self.fc1(x))
+        # x = self.relu(self.fc2(x))
+        # x = self.fc3(x)
+        # x = self.logsoftmax(x)
+        # return x
 
-        return y
-        raise NotImplementedError("You need to write this part!")
+        x = x.reshape(-1, 3, 31, 31)
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        x = x.view(-1, 64 * 3 * 3)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = self.logsoftmax(x)
+        return x
+
         ################## Your Code Ends here ##################
 
 
@@ -76,9 +109,9 @@ def fit(train_dataloader, test_dataloader, epochs):
     Please select an appropriate loss function from PyTorch torch.nn module.
     Please select an appropriate optimizer from PyTorch torch.optim module.
     """
-    loss_fn = None
-    optimizer = None
-    raise NotImplementedError("You need to write this part!")
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
     ################## Your Code Ends here ##################
 
 
@@ -115,7 +148,25 @@ def train(train_dataloader, model, loss_fn, optimizer):
 
     ################# Your Code Starts Here #################
 
-    raise NotImplementedError("You need to write this part!")
+    # Iterate over all the batches in dataloader:
+    #     1.  The model makes a prediction.
+    #     2.  Calculate the error in the prediction (loss).
+    #     3.  Zero the gradients of the optimizer.
+    #     4.  Perform backpropagation on the loss.
+    #     5.  Step the optimizer.
+
+    for batch, (X, y) in enumerate(train_dataloader):
+        y_pred = model(X)
+
+        loss = loss_fn(y_pred, y)
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+
     ################## Your Code Ends here ##################
 
 
